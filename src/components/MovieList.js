a3942cbf88e8a890cf6da7f1219187d2fc4670d0
Rijad Burović <rburovic1@etf.unsa.ba>
2023-07-28 16:36:54 +0200

    Displaying only movies that have posters

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,22 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 export default function MovieList({ movies, overlay, handleFavouritesClick }) {
   const Overlay = overlay;
 
   return (
     <>
-      {movies.map((movie) => (
-        <div
-          key={movie.imdbID}
-          className="d-flex justify-content-start m-3 image-container"
-          onClick={() => handleFavouritesClick(movie)}
-        >
-          <img src={movie.Poster} alt="movie" height="300"></img>
-          <div className="overlay ">
-            <Overlay />
+      {movies.map((movie) => {
+        if (movie.Poster === "N/A") return null;
+        return (
+          <div
+            key={movie.imdbID}
+            className="d-flex justify-content-start m-3 image-container"
+            onClick={() => handleFavouritesClick(movie)}
+          >
+            <img src={movie.Poster} alt="movie" height="350" width="auto"></img>
+            <div className="overlay ">
+              <Overlay />
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </>
   );
 }
